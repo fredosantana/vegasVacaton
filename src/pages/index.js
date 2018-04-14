3f0150b29219218720ee5b2f1527216a6a22d8fc
fredosantana <santana.wil@gmail.com>
2018-04-13 23:45:46 -0400

    DEPLOYEDgit add .

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ const IndexPage = () => (
   <div>
     <h2>Coming in June 2018!</h2>
     <p>See you soon Sin City!</p>
+    <img src="https://vegasexperience.com/wp-content/uploads/2017/12/Fremont-Street-Experience.jpg" alt="Downtown Las Vegas" />
+
   </div>
 )
 
