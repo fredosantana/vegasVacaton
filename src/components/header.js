d3e707ad7b5c20a30c2529a2ea08ffd53fb20755
fredosantana <santana.wil@gmail.com>
2018-04-16 13:09:46 -0400

    Gradient styles applied to header component and index layout

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,9 @@ import Link from 'gatsby-link'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: 'pink',
+      background: '#005AA7',  /* fallback for old browsers */
+      background: '-webkit-linear-gradient(to top, #FFFDE4, #005AA7)',  /* Chrome 10-25, Safari 5.1-6 */
+      background: 'linear-gradient(to top, #FFFDE4, #005AA7)', /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
       marginBottom: '1.45rem',
     }}
   >
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,7 +4,12 @@ import Helmet from 'react-helmet'
 import Header from '../components/header'
 
 const Layout = ({ children, data }) => (
-  <div>
+  <div style={{ 
+    background: '#a770ef',  /* fallback for old browsers */
+    background: '-webkit-linear-gradient(to top, #a770ef, #cf8bf3, #fdb99b)',  /* Chrome 10-25, Safari 5.1-6 */
+    background: 'linear-gradient(to top, #a770ef, #cf8bf3, #fdb99b)' /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    }}
+  >
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
